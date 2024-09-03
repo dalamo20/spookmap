27a090f80ef8dcb749067203c71555fd7d27b2de
dalamo20 <dalamo90@gmail.com>
2024-09-02 21:37:26 -0500

    test routing

---
+++ app/map/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react'
+
+const GoogleMaps = () => {
+  return (
+    <main>
+        <Link href={"/"}>Home</Link>
+        <div>GoogleMaps</div>
+    </main>
+  )
+}
+
+export default GoogleMaps;

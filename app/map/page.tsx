9c7d0cb16122e9e84eef94dd129ea01a8a077fd9
dalamo20 <dalamo90@gmail.com>
2024-09-02 23:09:31 -0500

    maps needs work

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,13 +1,42 @@
+'use client'
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+
+const mapStyles = {
+    width: '100%',
+    height: '50%'
+};
 
 const GoogleMaps = () => {
   return (
     <main>
         <Link href={"/"}>Home</Link>
         <div>GoogleMaps</div>
+        <Map
+            google={window.google}
+            zoom={17}
+            style={mapStyles}
+            initialCenter={
+                {
+                    lat: 19.020145856138136, 
+                    lng: -98.24006775697993
+                }
+            }
+        >
+            <Marker
+              position={
+                  {
+                      lat: 19.020145856138136, 
+                      lng: -98.24006775697993
+                  }
+              }
+           />
+        </Map>
     </main>
   )
 }
 
-export default GoogleMaps;
+export default GoogleApiWrapper({
+    apiKey: ""
+})(GoogleMaps);

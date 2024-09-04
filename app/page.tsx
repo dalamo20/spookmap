78dcac99a885c499ecac1123ad8fef5d5fb2e94b
dalamo20 <dalamo90@gmail.com>
2024-09-04 13:23:15 -0500

    checking remote changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>HEJ</h1>
+      <h1>Checking if branches work</h1>
       <Link href={"/map"}>Map</Link>
     </main>
   );
